9638ff59bb2cac631ae1ad8fbe43ed7cf5081368
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2021-01-06 15:07:39 +0100

    Channel filter

--- src/main/scala/com/github/frankivo/Discord.scala
+++ src/main/scala/com/github/frankivo/Discord.scala
@@ -1,10 +1,19 @@
 package com.github.frankivo
 
+import discord4j.common.util.Snowflake
 import discord4j.core.DiscordClient
 import discord4j.core.event.domain.message.MessageCreateEvent
 
 object Discord {
   val TOKEN: String = sys.env("DISCORD_TOKEN")
+
+  val CHANNELS: Seq[Long] = {
+    sys.env("DISCORD_CHANNELS")
+      .split(",")
+      .map(_.toLong)
+  }
+
+  def hasChannel(channel: Snowflake): Boolean = CHANNELS.contains(channel.asLong())
 }
 
 class Discord {
@@ -15,6 +24,7 @@ class Discord {
     val gateway = client.login().block()
 
     gateway.on(classOf[MessageCreateEvent])
+      .filter(e => Discord.hasChannel(e.getMessage.getChannelId))
       .toIterable
       .forEach(println)
   }
